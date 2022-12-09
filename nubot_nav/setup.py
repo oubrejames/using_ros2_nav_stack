de0eaa9e4941be8b3dba82f405286c82190e1a04
James Oubre <oubrejames@gmail.com>
2022-12-09 17:22:49 -0600

    Finished first part

---
+++ nubot_nav/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+package_name = 'nubot_nav'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml',
+                                   'config/nav2_params.yaml',
+                                   'config/nubot_nav_urdf.rviz',
+                                   'launch/manual_explore.launch.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='oubre',
+    maintainer_email='oubrejames@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
