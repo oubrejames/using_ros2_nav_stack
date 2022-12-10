47336b38982a3ee9379635cad2e7eb829948abd1
James Oubre <oubrejames@gmail.com>
2022-12-09 21:33:50 -0600

    Docstrings

--- nubot_nav/nubot_nav/explore.py
+++ nubot_nav/nubot_nav/explore.py
@@ -15,16 +15,23 @@ class Explore(Node):
         self.tmr = self.create_timer(20, self.timer_callback)
 
     def current_pose_cb(self, msg):
+        """Obtain pose from pose topic.
+        """
         self.pose_rn = msg
 
     def timer_callback(self):
+        """Iterate constantly.
+        """
+        # Create a goal pose for the robot to go to
         goal_pose = PoseStamped()
         goal_pose.header.frame_id = "map"
         goal_pose.header.stamp = self.get_clock().now().to_msg()
+        # Set goal pose to a random point away from its current pose
         goal_pose.pose.position.x = float(random.randint(-10,10)) + self.pose_rn.pose.pose.position.x
         goal_pose.pose.position.y = float(random.randint(-10,10)) + self.pose_rn.pose.pose.position.y
         goal_pose.pose.position.z = 0.0 
         goal_pose.pose.orientation.w = 1.0
+        # Publish
         self.goal_pub.publish(goal_pose)
 
 def main(args=None):
